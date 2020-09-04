7181c42b58ce44c47a36abbf1bca8261c8504306
Adrian Acosta <aa@ismeta.net>
2020-09-04 14:27:23 -0600

    Completes TC 2 page filters

--- test/pageobjects/searchresults.page.js
+++ test/pageobjects/searchresults.page.js
@@ -8,13 +8,19 @@ class SearchresultsPage extends Page {
      * define selectors using getter methods
      */
     get resultsView () { return $('.resultsView') }
+    get transportFilters () { return $('[data-test="HashtagHeader-transport"]') }
     
-    getResults () {
-        return this.resultsView.$('[data-test="WatchdogAndSortingWrapper"]');
-        //const noresult = this.resultsView.$('[class*=NoResultsstyled]');
-    }
+    getResults () { return this.resultsView.$('[data-test="WatchdogAndSortingWrapper"]') }    
+    getNoResults () { return this.resultsView.$('[class*=NoResultsstyled]') }
+    
+    filterCheckBus () { this.transportFilters.$('span=Bus').click() }
+    filterCheckTrain () { this.transportFilters.$('span=Train').click() }
+    filterCheckFlight () { this.transportFilters.$('span=Flight').click() }
 
-    
+    filterbyBus () {
+        this.filterCheckTrain();
+        this.filterCheckFlight();
+    }
 
      open () {  return super.open("us/search/results/berlin-tegel-berlin-germany/london-united-kingdom/2020-10-01_2020-10-31/2020-10-01_2020-10-31") }
 
--- test/specs/searchFilters.e2e.js
+++ test/specs/searchFilters.e2e.js
@@ -7,8 +7,15 @@ describe('Travel application', () => {
     it('should apply travel type filters', () => {
 
         SearchresultsPage.open();
-        //expect(SearchresultsPage.getResults()).toBeDisplayed();
-        //browser.pause(5000);
+        expect(SearchresultsPage.getResults()).toBeDisplayed();
+
+        SearchresultsPage.filterbyBus();
+        expect(SearchresultsPage.getNoResults()).toBeDisplayed();
+
+        SearchresultsPage.filterCheckTrain();
+        expect(SearchresultsPage.getResults()).toBeDisplayed();
+        
+        browser.pause(5000);
         
     });
 
