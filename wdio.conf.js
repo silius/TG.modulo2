e8bc3bf98b78ce5edb9e341c51014531928fdfde
Adrian Acosta <aa@ismeta.net>
2020-09-04 19:23:37 -0600

    Completes Project

--- wdio.conf.js
+++ wdio.conf.js
@@ -17,8 +17,8 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        // './test/specs/**/*.js'
-        './test/specs/bookaFlight.e2e.js'
+        './test/specs/**/*.js'
+        // './test/specs/bookaFlight.e2e.js'
     ],
     // Patterns to exclude.
     exclude: [
